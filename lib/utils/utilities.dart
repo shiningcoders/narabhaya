90dc4ec97e36a0c20c2cac35781ffdc321b35b57
Ajayraj Singh <shiningcoders@gmail.com>
2020-10-31 18:11:01 +0530

    Added utilities

---
+++ lib/utils/utilities.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:meta/meta.dart';
+
+class Utils {
+  static String getUsername(String email) {
+    return "live:${email.split('@')[0]}";
+  }
+
+  static String getInitials(String name) {
+    List<String> nameSplit = name.split(" ");
+    String firstNameInitial = nameSplit[0][0];
+    String lastNameInitial = nameSplit[1][0];
+    return firstNameInitial + lastNameInitial;
+  }
+
+  static String formatDateString(String dateString) {
+    DateTime dateTime = DateTime.parse(dateString);
+    var formatter = DateFormat('dd/MM/yy');
+    return formatter.format(dateTime);
+  }
+}
