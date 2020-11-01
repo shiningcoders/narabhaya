986955a0938eea34a7191a79aae8e467ecbe7c49
Ajayraj Singh <shiningcoders@gmail.com>
2020-11-01 12:57:55 +0530

    Test eolocation catching

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:narabhaya/resources/auth_methods.dart';
 import 'package:narabhaya/screens/login.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,63 +12,32 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthMethods _authMethods = AuthMethods();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Skype Clone",
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: LoginScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      initialRoute: '/',
+      routes: {
+        // '/search_screen': (context) => SearchScreen(),
+      },
+      home: FutureBuilder(
+        future: _authMethods.getCurrentUser(),
+        builder: (context, AsyncSnapshot<User> snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold();
+          } else {
+            return LoginScreen();
+          }
+        },
       ),
     );
   }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:location/location.dart';
 import 'package:narabhaya/resources/auth_methods.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -36,47 +40,326 @@ class LoginScreenState extends State<LoginScreen> {
       child: Text(
         "LOGIN",
         style: TextStyle(
-            fontSize: 35, fontWeight: FontWeight.w900, letterSpacing: 1.2),
+          fontSize: 35,
+          fontWeight: FontWeight.w900,
+          letterSpacing: 1.2,
+        ),
       ),
       onPressed: () => performLogin(),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
     );
   }
 
   void performLogin() async {
-    setState(() {
-      isLoginPressed = true;
-    });
+    setState(
+      () {
+        isLoginPressed = true;
+      },
+    );
 
     User user = await _authMethods.signIn();
 
     if (user != null) {
       authenticateUser(user);
     }
-    setState(() {
-      isLoginPressed = false;
-    });
+    setState(
+      () {
+        isLoginPressed = false;
+      },
+    );
   }
 
   void authenticateUser(User user) {
-    _authMethods.authenticateUser(user).then((isNewUser) {
-      setState(() {
-        isLoginPressed = false;
-      });
+    _authMethods.authenticateUser(user).then(
+      (isNewUser) {
+        setState(
+          () {
+            isLoginPressed = false;
+          },
+        );
 
-      if (isNewUser) {
-        _authMethods.addDataToDb(user).then((value) {
-          Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) {
-            return Scaffold();
-          }));
-        });
-      } else {
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) {
-          return Scaffold();
-        }));
-      }
+        if (isNewUser) {
+          _authMethods.addDataToDb(user).then(
+            (value) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return Scaffold();
+                  },
+                ),
+              );
+            },
+          );
+        } else {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return Scaffold(
+                  body: Container(
+                    padding: const EdgeInsets.all(32),
+                    child: Column(
+                      children: const <Widget>[
+                        PermissionStatusWidget(),
+                        Divider(height: 32),
+                        ServiceEnabledWidget(),
+                        Divider(height: 32),
+                        GetLocationWidget(),
+                        Divider(height: 32),
+                        ListenLocationWidget(),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+// Permission Status Widget
+
+class PermissionStatusWidget extends StatefulWidget {
+  const PermissionStatusWidget({Key key}) : super(key: key);
+
+  @override
+  _PermissionStatusState createState() => _PermissionStatusState();
+}
+
+class _PermissionStatusState extends State<PermissionStatusWidget> {
+  final Location location = Location();
+
+  PermissionStatus _permissionGranted;
+
+  Future<void> _checkPermissions() async {
+    final PermissionStatus permissionGrantedResult =
+        await location.hasPermission();
+    setState(() {
+      _permissionGranted = permissionGrantedResult;
     });
+  }
+
+  Future<void> _requestPermission() async {
+    if (_permissionGranted != PermissionStatus.granted) {
+      final PermissionStatus permissionRequestedResult =
+          await location.requestPermission();
+      setState(() {
+        _permissionGranted = permissionRequestedResult;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Permission status: ${_permissionGranted ?? "unknown"}',
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        Row(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 42),
+              child: RaisedButton(
+                child: const Text('Check'),
+                onPressed: _checkPermissions,
+              ),
+            ),
+            RaisedButton(
+              child: const Text('Request'),
+              onPressed: _permissionGranted == PermissionStatus.granted
+                  ? null
+                  : _requestPermission,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+// Service Enabled Widget
+
+class ServiceEnabledWidget extends StatefulWidget {
+  const ServiceEnabledWidget({Key key}) : super(key: key);
+
+  @override
+  _ServiceEnabledState createState() => _ServiceEnabledState();
+}
+
+class _ServiceEnabledState extends State<ServiceEnabledWidget> {
+  final Location location = Location();
+
+  bool _serviceEnabled;
+
+  Future<void> _checkService() async {
+    final bool serviceEnabledResult = await location.serviceEnabled();
+    setState(() {
+      _serviceEnabled = serviceEnabledResult;
+    });
+  }
+
+  Future<void> _requestService() async {
+    if (_serviceEnabled == null || !_serviceEnabled) {
+      final bool serviceRequestedResult = await location.requestService();
+      setState(() {
+        _serviceEnabled = serviceRequestedResult;
+      });
+      if (!serviceRequestedResult) {
+        return;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Service enabled: ${_serviceEnabled ?? "unknown"}',
+            style: Theme.of(context).textTheme.bodyText1),
+        Row(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 42),
+              child: RaisedButton(
+                child: const Text('Check'),
+                onPressed: _checkService,
+              ),
+            ),
+            RaisedButton(
+              child: const Text('Request'),
+              onPressed: _serviceEnabled == true ? null : _requestService,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+// GeoLoacation Widget
+
+class GetLocationWidget extends StatefulWidget {
+  const GetLocationWidget({Key key}) : super(key: key);
+
+  @override
+  _GetLocationState createState() => _GetLocationState();
+}
+
+class _GetLocationState extends State<GetLocationWidget> {
+  final Location location = Location();
+
+  LocationData _location;
+  String _error;
+
+  Future<void> _getLocation() async {
+    setState(() {
+      _error = null;
+    });
+    try {
+      final LocationData _locationResult = await location.getLocation();
+      setState(() {
+        _location = _locationResult;
+      });
+    } on PlatformException catch (err) {
+      setState(() {
+        _error = err.code;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Location: ' + (_error ?? '${_location ?? "unknown"}'),
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        Row(
+          children: <Widget>[
+            RaisedButton(
+              child: const Text('Get'),
+              onPressed: _getLocation,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+// Listen Location Widget
+
+class ListenLocationWidget extends StatefulWidget {
+  const ListenLocationWidget({Key key}) : super(key: key);
+
+  @override
+  _ListenLocationState createState() => _ListenLocationState();
+}
+
+class _ListenLocationState extends State<ListenLocationWidget> {
+  final Location location = Location();
+
+  LocationData _location;
+  StreamSubscription<LocationData> _locationSubscription;
+  String _error;
+
+  Future<void> _listenLocation() async {
+    _locationSubscription =
+        location.onLocationChanged.handleError((dynamic err) {
+      setState(() {
+        _error = err.code;
+      });
+      _locationSubscription.cancel();
+    }).listen((LocationData currentLocation) {
+      setState(() {
+        _error = null;
+
+        _location = currentLocation;
+      });
+    });
+  }
+
+  Future<void> _stopListen() async {
+    _locationSubscription.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Listen location: ' + (_error ?? '${_location ?? "unknown"}'),
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        Row(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 42),
+              child: RaisedButton(
+                child: const Text('Listen'),
+                onPressed: _listenLocation,
+              ),
+            ),
+            RaisedButton(
+              child: const Text('Stop'),
+              onPressed: _stopListen,
+            )
+          ],
+        ),
+      ],
+    );
   }
 }
