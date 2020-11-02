b822843726d9ca7749a33806f2a8116c12725c9e
Ajayraj Singh <shiningcoders@gmail.com>
2020-11-02 20:51:16 +0530

    Added Check Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:narabhaya/resources/auth_methods.dart';
+import 'package:narabhaya/screens/home.dart';
 import 'package:narabhaya/screens/login.dart';
 import 'package:provider/provider.dart';
 
@@ -29,16 +30,34 @@ class _MyAppState extends State<MyApp> {
       routes: {
         // '/search_screen': (context) => SearchScreen(),
       },
-      home: FutureBuilder(
-        future: _authMethods.getCurrentUser(),
-        builder: (context, AsyncSnapshot<User> snapshot) {
-          if (snapshot.hasData) {
-            return Scaffold();
-          } else {
-            return LoginScreen();
-          }
-        },
+      home: SplashScreen(
+        'assets/splash.flr',
+        CheckScreen(),
+        backgroundColor: Colors.white,
+        startAnimation: 'Intro',
       ),
+    );
+  }
+}
+
+class CheckScreen extends StatefulWidget {
+  @override
+  _CheckScreenState createState() => _CheckScreenState();
+}
+
+class _CheckScreenState extends State<CheckScreen> {
+  final AuthMethods _authMethods = AuthMethods();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _authMethods.getCurrentUser(),
+      builder: (context, AsyncSnapshot<User> snapshot) {
+        if (snapshot.hasData) {
+          return HomeScreen();
+        } else {
+          return LoginScreen();
+        }
+      },
     );
   }
 }
